44a8e9aeb579d6dac47fbd692e31ad1f7aa00255
abdelaleemadel <ahmedadel2912@gmail.com>
2025-04-14 16:47:41 +0200

    Add country page layout

--- app/country/[countryName]/page.js
+++ app/country/[countryName]/page.js
@@ -1,9 +1,88 @@
+import Image from "next/image";
 import ButtonBack from "../../_components/ButtonBack";
 
-function page() {
+async function page({ params }) {
+  const { countryName } = await params;
+  let data = await fetch(
+    `https://restcountries.com/v3.1/name/${countryName}?fullText=true&fields=name,tld,currencies,capital,region,subregion,languages,flags,borders,population`
+  );
+  let countryDetails = await data.json();
+  console.log(countryName, countryDetails);
+  let {
+    flags: { png: flag, alt },
+    name: { common: name },
+    nativeName,
+    tld,
+    currencies,
+    capital,
+    region,
+    subregion,
+    languages,
+    population,
+    borders,
+  } = countryDetails[0];
+
+  /*  `https://restcountries.com/v3.1/name/Belgium?fullText=true&fields=name,tld,currencies,capital,region,subregion,languages,flags,population` */
+
   return (
-    <div className="container border mx-auto">
+    <div className="container border mx-auto px-10">
       <ButtonBack />
+      <div className="border mt-10">
+        <div className="relative aspect-16/10">
+          <Image src={flag} alt={alt} fill className="object-cover" />
+        </div>
+        <div className="mt-5 py-5">
+          <div>
+            <p className="text-4xl font-black my-5">{name}</p>
+          </div>
+          <div>
+            <p>
+              <span className="font-bold">Native Name:</span>Belgie
+            </p>
+            <p>
+              <span className="font-bold">Population:</span>
+              {population}
+            </p>
+            <p>
+              <span className="font-bold">Region:</span>
+              {region}
+            </p>
+            <p>
+              <span className="font-bold">Sub Region:</span>
+              {subregion}
+            </p>
+            <p>
+              <span className="font-bold">Capital:</span>
+              {capital}
+            </p>
+          </div>
+
+          <div>
+            <p>
+              <span className="font-bold">Top Level Domain: </span>
+              {tld[0]}
+            </p>
+            <p>
+              <span className="font-bold">Currencies: </span>
+              {Object.keys(currencies).map(
+                (key, index) => `${index ? `, ` : ""}${currencies[key]["name"]}`
+              )}
+            </p>
+            <p>
+              <span className="font-bold">Languages: </span>
+              {Object.keys(languages).map(
+                (key, index) => `${index ? `, ` : ""}${languages[key]}`
+              )}
+            </p>
+            <p>
+              <span className="font-bold">Border Countries: </span>
+              {Object.keys(borders).map(
+                (key, index) => `${index ? `, ` : ""}${borders[key]}`
+              )}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
