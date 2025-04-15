e99dcb6a504098ce69d38670c0f843074480efac
abdelaleemadel <ahmedadel2912@gmail.com>
2025-04-15 18:37:02 +0200

    Get the search term from the input to the url search params

--- app/_components/Search.js
+++ app/_components/Search.js
@@ -1,12 +1,40 @@
+"use client";
+
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 function Search() {
+  const p = 5;
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const value = formData.get("search");
+    const params = new URLSearchParams(searchParams.toString());
+    if (value) {
+      params.set("search", value);
+    } else {
+      params.delete("search");
+    }
+    router.replace(`${pathname.toString()}?${params.toString()}`);
+  }
   return (
-    <div className="flex border-2 w-100 items-center p-1">
-      <FontAwesomeIcon className="me-3" icon={faMagnifyingGlass} />
-      <input className="w-[100%]" placeholder="search for a country..." />
-    </div>
+    <form
+      onSubmit={handleSubmit}
+      className="flex border-2 w-100 items-center p-1"
+    >
+      <button>
+        <FontAwesomeIcon className="me-3" icon={faMagnifyingGlass} />
+      </button>
+      <input
+        name="search"
+        className="w-[100%]"
+        placeholder="search for a country..."
+      />
+    </form>
   );
 }
 
