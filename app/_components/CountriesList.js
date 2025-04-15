f70a9040b4b122016cff17defc085460cb100df5
abdelaleemadel <ahmedadel2912@gmail.com>
2025-04-15 17:55:53 +0200

    Contect the fitler component to the url implementing the filter

--- app/_components/CountriesList.js
+++ app/_components/CountriesList.js
@@ -1,18 +1,25 @@
 import CountryItem from "./CountryItem";
 
 async function CountriesList({ region }) {
-  let link;
-  if (!region || region === "all") {
-    link =
-      "https://restcountries.com/v3.1/all?fields=name,population,capital,region,flags";
-  } else {
-    link = `https://restcountries.com/v3.1/region/${region}?fields=name,population,capital,region,flags`;
-  }
-  let data = await fetch(link);
+  let data = await fetch(
+    "https://restcountries.com/v3.1/all?fields=name,population,capital,region,flags"
+  );
   let countries = await data.json();
+  let displayed;
+  if (!region) region = "all";
+  if (
+    region === "Asia" ||
+    region === "Europe" ||
+    region === "Africa" ||
+    region === "Americas" ||
+    region === "Oceania" ||
+    region === "Antarctic"
+  ) {
+    displayed = countries.filter((country) => country.region === region);
+  } else {
+    displayed = countries;
+  }
 
-  let displayed = countries;
-  console.log(link);
   return (
     <div className="grid sm:grid-cols-1 px-10 mt-10 gap-8">
       {displayed &&
--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -1,15 +1,38 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
 function Filter() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  function handleSelection(e) {
+    const value = e.target.value;
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("region", value);
+    router.replace(`${pathname}?${params.toString()}`);
+    console.log(e.target.value);
+  }
   return (
     <div>
-      <select name="region" id="region" className="border">
+      <select
+        name="region"
+        id="region"
+        onChange={handleSelection}
+        className="border"
+      >
         <option value="" defaultValue hidden>
           Filter by Region
         </option>
-        <option value="africa">Africa</option>
-        <option value="america">America</option>
-        <option value="asia">Asia</option>
-        <option value="europe">Europe</option>
-        <option value="oceania">Oceania</option>
+        <option value="Africa">Africa</option>
+        <option value="Americas">Americas</option>
+        <option value="Asia">Asia</option>
+        <option value="Europe">Europe</option>
+        <option value="Oceania">Oceania</option>
+        <option value="Antarctic">Antarctic</option>
+        <option value="all">All</option>
       </select>
     </div>
   );
