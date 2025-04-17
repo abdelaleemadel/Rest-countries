49dbcad9c5dc30fdedb06527a9040fa006446651
abdelaleemadel <ahmedadel2912@gmail.com>
2025-04-17 16:17:07 +0200

    Implement light/dark mode toggle

--- app/_components/LightMode.js
+++ app/_components/LightMode.js
@@ -1,8 +1,15 @@
+"use client";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon } from "@fortawesome/free-regular-svg-icons";
 function LightMode() {
+  function handleToggle() {
+    document.documentElement.classList.toggle("dark");
+  }
   return (
-    <div className="text-2xl flex justify-center items-center">
+    <div
+      className="text-2xl flex justify-center items-center"
+      onClick={handleToggle}
+    >
       <FontAwesomeIcon icon={faMoon} className="mx-2" />
       <p>Light Mode</p>
     </div>
