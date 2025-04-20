76fe4191ba446c8d31b979a88c85361a2ab3b1e7
abdelaleemadel <ahmedadel2912@gmail.com>
2025-04-21 01:17:25 +0200

    Use local data at the home page in case of restcountries api error

---
+++ app/_lib/data-service.js
@@ -0,0 +1,33 @@
+"use server";
+async function getLocalCountries() {
+  console.log("local");
+  let res = await fetch("http://localhost:3000/data.json", {
+    cache: "force-cache",
+  });
+  let data = await res.json();
+  let localCountries = data.map((country) => {
+    return {
+      flags: { png: country.flags.png, alt: `The flag of ${country.name}` },
+      name: { common: country.name },
+      capital: [country.capital],
+      population: country.population,
+      region: country.region,
+    };
+  });
+  return localCountries;
+}
+
+export async function getCountries() {
+  let countries;
+  try {
+    let res = await fetch(
+      "https://restcountries.com/v3.11/all?fields=name,population,capital,region,flags"
+    );
+    if (res.status == 404) throw new Error("not found");
+    countries = await res.json();
+    if (!countries) throw new Error("not found");
+  } catch (error) {
+    countries = await getLocalCountries();
+  }
+  return countries;
+}
--- app/page.js
+++ app/page.js
@@ -3,20 +3,20 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 import Search from "./_components/Search";
 import SearchFilterBar from "./_components/SearchFilterBar";
 import CountriesList from "./_components/CountriesList";
+import { getAllCountries, getCountries } from "./_lib/data-service";
 
 export default async function Home({ searchParams }) {
   const { region, search } = await searchParams;
-
+  let countries;
   let link =
     "https://restcountries.com/v3.1/all?fields=name,population,capital,region,flags";
   if (search) {
     link = `https://restcountries.com/v3.1/name/${search}?fields=name,population,capital,region,flags`;
+  } else {
+    countries = await getCountries();
   }
 
-  let data = await fetch(link);
-  let countries = await data.json();
-
-  console.log(region);
+  console.log(countries[0]);
   return (
     <>
       <SearchFilterBar />
