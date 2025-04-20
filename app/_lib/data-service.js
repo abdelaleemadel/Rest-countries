c6a2d664b81cf5685225ea1b053d897f35e29348
abdelaleemadel <ahmedadel2912@gmail.com>
2025-04-21 01:54:18 +0200

    Use search with local data when the api server goes down

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -20,15 +20,36 @@ async function getLocalCountries() {
 export async function getCountries() {
   let countries;
   try {
-    throw new Error("");
+    /*  throw new Error("") */
     let res = await fetch(
       "https://restcountries.com/v3.1/all?fields=name,population,capital,region,flags"
     );
-    if (res.status == 404) throw new Error("not found");
+    if (res.status != 200) throw new Error("not found");
     countries = await res.json();
     if (!countries) throw new Error("not found");
   } catch (error) {
     countries = await getLocalCountries();
   }
+  return countries;
+}
+
+export async function searchCountries(input) {
+  let countries;
+  input = input.toLowerCase();
+
+  try {
+    let res = await fetch(
+      `https://restcountries.com/v3.1/name/${input}?fields=name,population,capital,region,flags`,
+      { next: { revalidate: 86400 } }
+    );
+    if (res.status != 200) throw new Error("not found");
+    countries = await res.json();
+  } catch (error) {
+    let allCountries = await getLocalCountries();
+    countries = allCountries.filter((country) =>
+      country.name.common.toLowerCase().includes(input)
+    );
+  }
+
   return countries;
 }
--- app/page.js
+++ app/page.js
@@ -3,20 +3,18 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 import Search from "./_components/Search";
 import SearchFilterBar from "./_components/SearchFilterBar";
 import CountriesList from "./_components/CountriesList";
-import { getAllCountries, getCountries } from "./_lib/data-service";
+import { getCountries, searchCountries } from "./_lib/data-service";
 
 export default async function Home({ searchParams }) {
   const { region, search } = await searchParams;
   let countries;
-  let link =
-    "https://restcountries.com/v3.1/all?fields=name,population,capital,region,flags";
+
   if (search) {
-    link = `https://restcountries.com/v3.1/name/${search}?fields=name,population,capital,region,flags`;
+    countries = await searchCountries(search);
   } else {
     countries = await getCountries();
   }
 
-  console.log(countries[0]);
   return (
     <>
       <SearchFilterBar />
