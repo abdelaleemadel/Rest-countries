f5cf84cadc6037cb6ececdebee62091987f3d92b
abdelaleemadel <ahmedadel2912@gmail.com>
2025-04-23 01:02:24 +0200

    Refactor fetching functions to fetch only once

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,108 +1,74 @@
-async function getLocalCountries({ countryName, countryCode }) {
-  console.log("local");
+async function getLocalCountries() {
   let res = await fetch("http://localhost:3000/data.json", {
     cache: "force-cache",
   });
-  let data = await res.json();
-  let countries = data;
-  let localCountries;
-  if (countryName) {
-    countries = [data.find((country) => country.name === countryName)];
-    localCountries = countries.map((country) => {
-      return {
-        flags: { png: country.flags.png, alt: `The flag of ${country.name}` },
-        name: { common: country.name, nativeName: country.nativeName },
-        capital: [country.capital],
-        population: country.population,
-        region: country.region,
-        tld: country.topLevelDomain,
-        subregion: country.subregion,
-        borders: country.borders,
-        languages: country.languages,
-        currencies: country.currencies,
-      };
-    });
-  } else if (countryCode) {
-    let country = data.find((country) => countryCode === country.alpha3Code);
-    localCountries = { name: { common: country.name } };
-  } else {
-    localCountries = countries.map((country) => {
-      return {
-        flags: { png: country.flags.png, alt: `The flag of ${country.name}` },
-        name: { common: country.name },
-        capital: [country.capital],
-        population: country.population,
-        region: country.region,
-      };
-    });
-  }
+  let countries = await res.json();
+  let localCountries = countries.map((country) => {
+    return {
+      flags: { png: country.flags.png, alt: `The flag of ${country.name}` },
+      name: { common: country.name, nativeName: country.nativeName },
+      capital: [country.capital],
+      population: country.population,
+      region: country.region,
+      tld: country.topLevelDomain,
+      subregion: country.subregion,
+      borders: country.borders,
+      languages: country.languages,
+      currencies: country.currencies,
+      cca3: country.alpha3Code,
+    };
+  });
 
   return localCountries;
 }
 
-export async function getCountries() {
+async function getAllCountries() {
   let countries;
   try {
-    /*  throw new Error("") */
     let res = await fetch(
-      "https://restcountries.com/v3.1/all?fields=name,population,capital,region,flags"
+      "https://restcountries.com/v3.1/all?fields=name,tld,currencies,capital,region,subregion,languages,flags,borders,population,cca3"
     );
     if (res.status != 200) throw new Error("not found");
     countries = await res.json();
     if (!countries) throw new Error("not found");
   } catch (error) {
+    console.log("localAll");
+
     countries = await getLocalCountries();
   }
   return countries;
 }
 
-export async function searchCountries(input) {
-  let countries;
-  input = input.toLowerCase();
+const allCountries = getAllCountries();
 
-  try {
-    let res = await fetch(
-      `https://restcountries.com/v3.1/name/${input}?fields=name,population,capital,region,flags`,
-      { next: { revalidate: 86400 } }
-    );
-    if (res.status != 200) throw new Error("not found");
-    countries = await res.json();
-  } catch (error) {
-    let allCountries = await getLocalCountries();
-    countries = allCountries.filter((country) =>
-      country.name.common.toLowerCase().includes(input)
-    );
-  }
+export async function getCountries() {
+  let countries = await allCountries;
+  return countries;
+}
+
+export async function searchCountries(input) {
+  input = input.toLowerCase();
+  const countries = await allCountries;
+
+  let resultCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(input)
+  );
 
   return countries;
 }
 
 export async function getCountryDetails(countryName) {
-  let country;
-  try {
-    /* throw new Error(""); */
-    let data = await fetch(
-      `https://restcountries.com/v3.1/name/${countryName}?fullText=true&fields=name,tld,currencies,capital,region,subregion,languages,flags,borders,population`
-    );
-    if (res.status != 200) throw new Error("not found");
-    country = await data.json();
-  } catch (error) {
-    country = await getLocalCountries({ countryName });
-  }
-  return country;
+  const countries = await allCountries;
+  const country = countries.find(
+    (country) => country.name.common === countryName
+  );
+  return [country];
 }
 
 export async function getBorderCountry(countryCode) {
-  let country;
-  try {
-    /* throw new Error(""); */
-    let data = await fetch(
-      `https://restcountries.com/v3.1/alpha/${countryCode}?fields=name`
-    );
-    if (res.status != 200) throw new Error("not found");
-    country = await data.json();
-  } catch (error) {
-    country = await getLocalCountries({ countryCode });
-  }
-  return country;
+  const countries = await allCountries;
+
+  let country = countries.find((country) => country.cca3 === countryCode);
+  console.log(countryCode, country);
+  return { name: { common: country.name.common } };
 }
