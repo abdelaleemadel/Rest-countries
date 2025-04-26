5e1f60b7d6e3b0a65c991e2b52f32aaf5aa0cb84
abdelaleemadel <ahmedadel2912@gmail.com>
2025-04-26 03:08:21 +0300

    Remove consolelog from home page

--- app/page.js
+++ app/page.js
@@ -8,12 +8,9 @@ import { getCountries, searchCountries } from "./_lib/data-service";
 export default async function Home({ searchParams }) {
   const { region, search } = await searchParams;
   let countries;
-  console.log(search);
   if (search) {
-    console.log("in search");
     countries = await searchCountries(search);
   } else {
-    console.log("no search");
     countries = await getCountries();
   }
 
