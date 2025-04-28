28a45b03b0f394072c77269f26dcf4d07db587e5
abdelaleemadel <ahmedadel2912@gmail.com>
2025-04-29 02:40:54 +0300

    Make the title go back to the home page with no filters or searches

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import LightMode from "./LightMode";
 
 function Header() {
   return (
     <header className="shadow dark:bg-dark-blue dark:text-white  absolute left-0 right-0 top-0 bg-white  items-center">
       <div className="flex justify-between max-w-9/10  mx-auto  h-25  items-center">
-        <h1 className="font-black text-xl ">Where in the world?</h1>
+        <Link href="/">
+          <h1 className="font-black text-xl ">Where in the world?</h1>
+        </Link>
         <LightMode />
       </div>
     </header>
