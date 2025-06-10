db6608a5d907f9b4cbaadd21c8404599f70ebbec
abdelaleemadel <ahmedadel2912@gmail.com>
2025-06-10 20:27:23 +0300

    Set vercel configurations

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -5,8 +5,10 @@ function Header() {
   return (
     <header className="shadow dark:bg-dark-blue dark:text-white  absolute left-0 right-0 top-0 bg-white  items-center">
       <div className="flex justify-between max-w-9/10  mx-auto  h-25  items-center">
-        <Link href="/">
-          <h1 className="font-black text-xl ">Where in the world?</h1>
+        <Link href="/" className="focus-visible:outline-none">
+          <h1 className="font-black text-md sm:text-xl focus-visible:outline-none">
+            Where in the world?
+          </h1>
         </Link>
         <LightMode />
       </div>
