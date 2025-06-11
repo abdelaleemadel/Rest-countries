01045f78f8952317adbac670032eb53b52b55116
abdelaleemadel <ahmedadel2912@gmail.com>
2025-06-11 19:00:24 +0300

    Add spinner to loading page

--- app/loading.js
+++ app/loading.js
@@ -1,5 +1,12 @@
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function loading() {
-  return <div>Loading</div>;
+  return (
+    <div className="w-full text-9xl text-center my-[30%]">
+      <FontAwesomeIcon icon={faCircleNotch} className="animate-spin" />
+    </div>
+  );
 }
 
 export default loading;
--- app/page.js
+++ app/page.js
@@ -1,10 +1,7 @@
-import Image from "next/image";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import Search from "./_components/Search";
 import SearchFilterBar from "./_components/SearchFilterBar";
 import CountriesList from "./_components/CountriesList";
 import { getCountries, searchCountries } from "./_lib/data-service";
-import { cookies } from "next/headers";
 
 export default async function Home({ searchParams }) {
   const { region, search } = await searchParams;
