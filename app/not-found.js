b5a298b0234c1c5f9747934e1dd2eff8965f7453
abdelaleemadel <ahmedadel2912@gmail.com>
2025-06-20 06:12:51 +0300

    Change not-found elements size in different screen sizes

--- app/not-found.js
+++ app/not-found.js
@@ -3,9 +3,11 @@ import Link from "next/link";
 function NotFound() {
   return (
     <div className="my-32 text-center dark:bg-very-dark dark:text-white">
-      <h3 className="text-8xl font-extrabold">Error 404</h3>
-      <p className="text-4xl font-bold my-16">This Page Does Not Exist</p>
-      <Link href="/" className="border py-3 px-5 rounded">
+      <h3 className="text-6xl sm:text-8xl font-extrabold">Error 404</h3>
+      <p className="text-2xl sm:text-4xl font-bold my-16">
+        This Page Does Not Exist
+      </p>
+      <Link href="/" className="border py-3 px-5 text-xl lg:text-2xl rounded">
         Home Page
       </Link>
     </div>
